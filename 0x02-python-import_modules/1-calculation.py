0418b34faa6de66ff503cd15527e1add0f7b8301
eyobsis <ehtube2020@gmail.com>
2023-08-10 15:53:49 -0700

    commit

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+from calculator_1 import add, sub, mul, div
+
+output = add(a, b)
+print(f"{a} + {b} = {output}")
+output = add(a, b)
+print(f"{a} - {b} = {output}")
+output = add(a, b)
+print(f"{a} * {b} = {output}")
+output = add(a, b)
+print(f"{a} / {b} = {output}")
