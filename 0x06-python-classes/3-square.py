7ac13958eb9689daa278d50f10d177d1e600b552
eyobsis <ehtube2020@gmail.com>
2023-08-29 14:21:11 -0700

    commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class represents a square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the current area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
